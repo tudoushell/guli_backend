f04300a42582fa9bd7d3adf1e8134a59defbb1d1
happy <whitehatzk520@gmail.com>
2020-09-23 23:32:07 +0800

    spring security 配置

--- common/spring_security/src/main/java/com/elliot/security/config/WebSecurityConfig.java
+++ common/spring_security/src/main/java/com/elliot/security/config/WebSecurityConfig.java
@@ -1,18 +1,22 @@
 package com.elliot.security.config;
 
+import com.elliot.security.security.UnauthorizedEntryPoint;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author elliot
  * @since 2020-09-23
  */
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-
+    http.exceptionHandling()
+            .authenticationEntryPoint(new UnauthorizedEntryPoint());
   }
 
   /**
--- service/service_acl/src/main/java/com/elliot/acl/service/impl/UserDetailsServiceImpl.java
+++ service/service_acl/src/main/java/com/elliot/acl/service/impl/UserDetailsServiceImpl.java
@@ -3,11 +3,13 @@ package com.elliot.acl.service.impl;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 /**
  * @author elliot
  * @since 2020-09-20
  */
+@Component("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
   @Override
   public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
